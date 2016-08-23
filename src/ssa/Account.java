85d52d6eaf1c36f37f115c40a0b42087faa3562d
DanielJ18 <atochala20@gmail.com>
2016-08-23 16:28:49 -0400

    Account class verision 2

--- src/ssa/Account.java
+++ src/ssa/Account.java
@@ -6,14 +6,63 @@ public class Account {
     private String description;
     private double balance;
 
+    // Account account1 = new Account();
+    // Account account2 = new Account();
+
+    // constructors for class Account
+
+    public Account() {
+	this.balance = 0;
+    }
+
+    public Account(String description) {
+	this.description = description;
+
+    }
+
+    public Account(int accountId, String description) {
+	this.accountId = accountId;
+	this.description = description;
+
+    }
+
+    // print method
+
+//    public void print() {
+//	
+//	System.out.printf("Accout: %d, Balance: %.2f \n", getAccountId(), getBalance());
+//	System.out.println("--------------------------------------------------");
+//
+//    }
+    
+    public String print()
+    {
+	String thisIsWhatIwantAndIwantItNow = "Account " + getAccountId() + " balance is $" + getBalance();
+	
+	return thisIsWhatIwantAndIwantItNow;
+    }
+
+    // Transfer $$ method
+
+    public void transferFrom(Account accountName, double transferAmount) {
+
+	if (transferAmount <= accountName.getBalance()) {
+	    accountName.withdraw(transferAmount);
+	    this.deposit(transferAmount);
+	}
+	
+	else
+	    System.out.println("Insufficent funds. Enter the proper transfer amount.");
+
+    }
 
     // method to perform deposit
     public void deposit(double depositAmount) {
 
 	this.balance += depositAmount;
 
-	System.out.printf("Deposit Amount: %.2f Balance: %.2f\n", depositAmount, this.balance);
-	System.out.println("---------------------------------------");
+	System.out.printf("Accout: %d, deposit: %.2f balance: %.2f\n", getAccountId(), depositAmount, this.balance);
+	System.out.println("--------------------------------------------------");
 
     }
 
@@ -24,16 +73,16 @@ public class Account {
 
 	    System.out.println("Insufficent funds." + "\nYour withdrawal of: " + withdrawAmount + " NOT approved. "
 		    + "\nYour balance is: " + this.balance);
-	    System.out.println("---------------------------------------");
+	    System.out.println("--------------------------------------------------");
 	}
 
 	else
 
 	{
 	    this.balance = this.balance - withdrawAmount;
-	    System.out.printf("Withdrawal Amount: %.2f  Balance: %.2f\n", withdrawAmount, this.balance);
+	    System.out.printf("Account: %d Withdrawal: %.2f  balance: %.2f\n", getAccountId(), withdrawAmount, this.balance);
 
-	    System.out.println("---------------------------------------");
+	    System.out.println("--------------------------------------------------");
 
 	}
 
@@ -45,7 +94,7 @@ public class Account {
 	return accountId;
     }
 
-    public void setAccountId(int accountId) {
+    private void setAccountId(int accountId) {
 	this.accountId = accountId;
     }
 
@@ -61,9 +110,8 @@ public class Account {
 	return balance;
     }
 
-    public void setBalance(double balance) {
+    private void setBalance(double balance) {
 	this.balance = balance;
     }
-
 
 }
--- src/ssa/Mainline.java
+++ src/ssa/Mainline.java
@@ -5,14 +5,14 @@ public class Mainline {
     public static void main(String[] args) {
 
 	// creating a checking and saving objects
-	Account checking = new Account();
-	Account savings = new Account();
+	Account checking = new Account(100, "My personal  account");
+	Account savings = new Account(200, "My personal  account");
 
-	checking.setAccountId(100);
+//	checking.setAccountId(100);
 	
 
 	checking.setDescription("My personal checking account");
-	checking.setBalance(500.00);
+	//checking.setBalance(500.00);
 
 	System.out.println(checking.getDescription() + "\nAccount ID: " + checking.getAccountId() + "\nInitial Balance: "
 		+ checking.getBalance());
@@ -27,21 +27,22 @@ public class Mainline {
 	checking.withdraw(600.00);
 	checking.getBalance();
 
-	checking.deposit(100.00);
+	checking.deposit(1000.00);
 	checking.withdraw(300.00);
 	checking.getBalance();
 
 	checking.withdraw(200.00);
 	checking.getBalance();
-
+	
+	
 	// Savings
 	System.out.println("\n\n");
 
-	savings.setAccountId(200);
+//	savings.setAccountId(200);
 
 
 	savings.setDescription("My personal savings account");
-	savings.setBalance(1000.00);
+//	savings.setBalance(1000.00);
 
 	System.out.println(savings.getDescription() + "\nAccount ID: " + savings.getAccountId() + "\nInitial Balance: "
 		+ savings.getBalance());
@@ -56,8 +57,13 @@ public class Mainline {
 
 	savings.deposit(200.00);
 	savings.getBalance();
-
-
+	
+	
+	System.out.println(savings.print());
+	checking.print();
+	
+	checking.transferFrom(savings, 100);
+	
     }
 
 
